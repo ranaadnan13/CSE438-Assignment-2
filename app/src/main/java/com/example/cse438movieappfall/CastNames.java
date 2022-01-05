9862bba66ad5c9ba41b84cef7dc70c82dd1988b8
ranaadnan13 <radnanmishal@gmail.com>
2022-01-06 00:17:23 +0600

    Init

---
+++ app/src/main/java/com/example/cse438movieappfall/CastNames.java
@@ -0,0 +1,44 @@
+package com.example.cse438movieappfall;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class CastNames implements Serializable {
+    @SerializedName("id")
+    @Expose
+    private long id;
+    @SerializedName("cast")
+    @Expose
+    private List<Cast> cast = null;
+    @SerializedName("crew")
+    @Expose
+    private List<Cast> crew = null;
+    private final static long serialVersionUID = 1227730618350775398L;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Cast> getCast() {
+        return cast;
+    }
+
+    public void setCast(List<Cast> cast) {
+        this.cast = cast;
+    }
+
+    public List<Cast> getCrew() {
+        return crew;
+    }
+
+    public void setCrew(List<Cast> crew) {
+        this.crew = crew;
+    }
+}
